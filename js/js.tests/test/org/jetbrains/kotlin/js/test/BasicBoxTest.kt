bdd1b8dc53bfd89588c9521cce65471d5107102c
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2020-06-03 01:47:32 +0300

    ~~~
    * process based engines
    * add V8
    * simplify ScriptEngine
    * extract ScriptEngineWithTypedResult mostly for JsReplEvaluator and JsScriptEvaluator
    * remove ScriptEngineV8.kt J2V8 based
    * use new ScriptEngineV8 everywhere* by default
    ** engineForMinifier
    ** BasicOptimizerTest.kt

--- js/js.tests/test/org/jetbrains/kotlin/js/test/BasicBoxTest.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/BasicBoxTest.kt
@@ -32,8 +32,6 @@ import org.jetbrains.kotlin.js.config.*
 import org.jetbrains.kotlin.js.dce.DeadCodeElimination
 import org.jetbrains.kotlin.js.dce.InputFile
 import org.jetbrains.kotlin.js.dce.InputResource
-import org.jetbrains.kotlin.js.engine.ScriptEngineNashorn
-import org.jetbrains.kotlin.js.engine.ScriptEngineV8Lazy
 import org.jetbrains.kotlin.js.facade.*
 import org.jetbrains.kotlin.js.parser.parse
 import org.jetbrains.kotlin.js.parser.sourcemaps.SourceMapError
@@ -860,7 +858,7 @@ abstract class BasicBoxTest(
         val result = engineForMinifier.runAndRestoreContext {
             runList.forEach(this::loadFile)
             overrideAsserter()
-            eval<String>(SETUP_KOTLIN_OUTPUT)
+            eval(SETUP_KOTLIN_OUTPUT)
             runTestFunction(testModuleName, testPackage, testFunction, withModuleSystem)
         }
         TestCase.assertEquals(expectedResult, result)
@@ -1027,9 +1025,7 @@ abstract class BasicBoxTest(
         private val OLD_MODULE_SUFFIX = "-old"
 
         const val KOTLIN_TEST_INTERNAL = "\$kotlin_test_internal\$"
-        private val engineForMinifier =
-            if (runTestInNashorn) ScriptEngineNashorn()
-            else ScriptEngineV8Lazy(KotlinTestUtils.tmpDirForReusableFolder("j2v8_library_path").path)
+        private val engineForMinifier = createScriptEngine()
     }
 }
 
