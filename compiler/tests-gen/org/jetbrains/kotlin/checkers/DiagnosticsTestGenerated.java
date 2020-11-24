a98019ef20dc77275734b66e0f9e9d6d4ef1d64d
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-11-24 14:42:10 +0300

    [FE] Prohibit inheritance of sealed classes in different module
    KT-20423

--- compiler/tests-gen/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -20825,6 +20825,11 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTestWithFirVali
                 runTest("compiler/testData/diagnostics/tests/sealed/ExhaustiveWithFreedom.kt");
             }
 
+            @TestMetadata("inheritorInDifferentModule.kt")
+            public void testInheritorInDifferentModule() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/sealed/inheritorInDifferentModule.kt");
+            }
+
             @TestMetadata("Local.kt")
             public void testLocal() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/sealed/Local.kt");
@@ -20960,6 +20965,11 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTestWithFirVali
 
                 public void testAllFilesPresentInInterfaces() throws Exception {
                     KotlinTestUtils.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/diagnostics/tests/sealed/interfaces"), Pattern.compile("^(.*)\\.kts?$"), Pattern.compile("^(.+)\\.fir\\.kts?$"), true);
+                }
+
+                @TestMetadata("inheritorInDifferentModule.kt")
+                public void testInheritorInDifferentModule() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/sealed/interfaces/inheritorInDifferentModule.kt");
                 }
 
                 @TestMetadata("sealedInterfacesDisabled.kt")
