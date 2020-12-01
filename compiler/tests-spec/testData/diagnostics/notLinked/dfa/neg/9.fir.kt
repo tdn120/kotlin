fdb8e67c565ec11a998e9771e7353a29f92fa4a6
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-12-01 14:33:00 +0300

    ~~~ Questionable test data change ~~~

--- compiler/tests-spec/testData/diagnostics/notLinked/dfa/neg/9.fir.kt
+++ compiler/tests-spec/testData/diagnostics/notLinked/dfa/neg/9.fir.kt
@@ -9,8 +9,8 @@ fun <T, K> case_1(x: T?, y: K?) {
     val z = <!DEBUG_INFO_EXPRESSION_TYPE("T & T?")!>x<!> ?: <!DEBUG_INFO_EXPRESSION_TYPE("K & K?")!>y<!>
 
     <!DEBUG_INFO_EXPRESSION_TYPE("T & T?")!>x<!>.<!INAPPLICABLE_CANDIDATE!>equals<!>(10)
-    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Any?")!>z<!>
-    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Any?")!>z<!>.<!INAPPLICABLE_CANDIDATE!>equals<!>(10)
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Any")!>z<!>
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Any")!>z<!>.equals(10)
 }
 
 // TESTCASE NUMBER: 1
